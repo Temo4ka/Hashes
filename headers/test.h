8ca6981df78b76081fc3424ec8b48b6493d28a65
Temo4ka Bukin <artemy.bukin@yandex.ru>
2023-05-15 13:32:01 +0300

    codeStyle changes and better organization

--- headers/test.h
+++ headers/test.h
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 #include "config.h"
 #include "hash.h"
 #include "in.h"
 
 int test_diagrams(Text *text, const char* outputFileName);
 
-=======
-#include "config.h"
-#include "hash.h"
-#include "in.h"
-
-int test_diagrams(HashFunc_t hashFuncs[HASH_FUNC_NUM], Text *text, const char* outputFileName, size_t hashNum);
-
->>>>>>> 4a0a211e26fa9b71d07de0bc5cf7e43c6228120f
 int test_speed(HashFunc_t hash, Text *text);
